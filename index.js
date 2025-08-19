6549a4d3af62cb7c975136b29d348452a4e002fc
Dhanashree-Thube <dhanashreethube11@gmail.com>
2025-08-19 14:59:03 +0530

    added new one

---
+++ index.js
@@ -0,0 +1,23 @@
+//primitive type
+
+let name ="omkar";
+let age = name;
+console.log(name);
+console.log(age);
+name="thube";
+console.log(name);
+console.log(age);
+
+
+
+// reference type
+
+let obj ={
+    name:"omkar",
+    age:25,
+}
+
+let obj1 = obj;
+obj1.name ="sonu";
+console.log(obj);
+console.log(obj1);
